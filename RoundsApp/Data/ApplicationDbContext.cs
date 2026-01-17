85051966b62967d5ca9e1aa25cbca192cf6ee99e
Jere <jere.niemi@solteq.com>
2026-01-17 13:54:29 +0200

    Add ApplicationDbContext and ApplicationUser models for identity management

---
+++ RoundsApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RoundsApp.Models;
+
+namespace RoundsApp.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Customize Identity table names if needed
+        builder.Entity<ApplicationUser>(entity =>
+        {
+            entity.ToTable(name: "Users");
+        });
+    }
+}
